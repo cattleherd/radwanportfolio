3bbe60b2180e8dc75b1ec6b38099545f4710ee82
Radwan Ahmed <radwan.hussein@protonmail.com>
2022-08-02 11:09:09 -0400

    fix issues

--- public/js/app.js
+++ public/js/app.js
@@ -4,20 +4,38 @@ const menu = document.querySelector('#menu')
 const menulink = document.querySelector('#menu-link')
 const mainmenus = document.querySelector('#mainmenu')
 const hamburgeritem = document.querySelectorAll('#hamburgeritem') //grabs all elements with that id not just first
+const skillbar = document.querySelectorAll('.skill')
 
-const contactform = document.querySelector('#contact-form')
+const contactform = document.querySelector('#contact')
 let name = document.getElementById('fullname')
 let email = document.getElementById('email')
 let message = document.getElementById('message')
 
+let modalbutton = document.getElementById('hello')
+const modal = document.getElementById('modal')
+const close = document.getElementById('closemodal')
+
+
 //menu dropdown functionality
-hamburger.addEventListener('click', ()=>{
+hamburger.addEventListener('click', (e)=>{
     //when clicked  main menu becomes hidden
     menu.classList.toggle('hidden')
     hamburgeritem.forEach(e=>{
         //each tick in hamburger menu becomes white on toggle with click
         e.classList.toggle('bg-white')
     })
+    e.stopPropagation() //this is added so that all other click events wont be triggered at the same time
+})
+
+document.body.addEventListener('click',(e)=>{
+    menu.classList.add('hidden')
+    modal.classList.add('hidden')
+    e.stopPropagation()
+})
+
+close.addEventListener('click', (e)=>{
+    modal.classList.add('hidden')
+    e.stopPropagation()
 })
 
 //post form data
@@ -41,8 +59,9 @@ contactform.addEventListener('submit', (e)=>{
             })
         }
         fetchdata().then((req,res)=>{
-            console.log(res)
+            console.log('succes')
         })
+        modal.classList.toggle('hidden')
     }catch(err){console.log(err)}
 
 })
